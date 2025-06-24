9e0ff0d4aff71b196cea524dec19314701697984
Documenter.jl <documenter@juliadocs.github.io>
2025-06-24 09:49:18 +0000

    build based on 02b48d7

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#MATH582","page":"Home","title":"MATH582","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MATH582.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#MATH582","page":"Home","title":"MATH582","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MATH582.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
